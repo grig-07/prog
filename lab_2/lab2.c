4c273e3206482f95c9a7df368c4ea51a3e5445f6
grig-07 <vi8518200@gmail.com>
2024-10-28 09:57:23 +0500

    Update lab2.c

--- lab_2/lab2.c
+++ lab_2/lab2.c
@@ -1,2 +1,19 @@
-include <math.h>
-include <stdio.h>
+#include <math.h>
+#include <stdio.h>
+int main(){
+    double h;
+    FILE *graph;
+    printf(Enter h: ");
+    scanf("%lf", &h);
+    graph = fopen("graph.txt", "w");
+      for (double x=0;x<=2;x=x+h;){
+          if (x <= 2){
+              fprinf(graph, "%lf\t%lf\n", x, sqrt(x+1) - sqrt(x) - 0,5);
+              prinf("%lf\t%lf\n", x, sqrt(x+1) - sqrt(x) - 0,5);
+          }else{
+              fprinf(graph, "%lf\t%lf\n", x, exp(-x - 1/x));
+          }
+          fclose(graph);
+          return 0;
+      }
+}
